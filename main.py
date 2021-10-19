83701c1393d1e6956594e6e3a5118aa66171f920
gavrielbenny <18219078@std.stei.itb.ac.id>
2021-10-19 16:34:26 +0700

    Final

--- main.py
+++ main.py
@@ -1,15 +1,40 @@
 import json
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Depends
+
+from auth.auth_handler import signJWT, get_password_hash, verify_password
+from auth.auth_bearer import JWTBearer
 
 with open("menu.json", "r") as read_file:
     data = json.load(read_file)
+with open("user.json", "r") as read_file2:
+    data2 = json.load(read_file2)
+
 app = FastAPI()
+
+@app.post('/user/register')
+async def write_user(username : str, password : str):
+    hashing = get_password_hash(password)
+    newdata = {'username': username, 'password' : hashing}
+    data2.append(newdata)
+    with open("user.json", "w") as write_file:
+        json.dump(data2, write_file)
+    write_file.close()
+    return data2
+
+@app.post('/user/login')
+async def write_user(username : str, password : str):
+    for data_user in data2:
+        if data_user['username'] == username:
+            if verify_password(password, data_user['password']):
+                return signJWT(username)
+            else :
+                return('login failed')
 
 @app.get('/')
 def root():
     return{'Menu':'Item'}
 
-@app.get('/menu/{item_id}')
+@app.get('/menu/{item_id}', dependencies=[Depends(JWTBearer())])
 async def read_menu(item_id: int):
     for menu_item in data['menu']:
         if menu_item['id'] == item_id:
@@ -18,7 +43,7 @@ async def read_menu(item_id: int):
         status_code=404, detail=f'Item not found'
         )
 
-@app.post('/menu/{item_id}/{item_name}')
+@app.post('/menu/{item_id}/{item_name}', dependencies=[Depends(JWTBearer())])
 async def write_menu(name: str):
     item_id = len(data['menu'])+1
     newdata = {'id': item_id, 'name' : name}
@@ -29,7 +54,7 @@ async def write_menu(name: str):
         write_file.close()
         return data
         
-@app.put('/menu/{item_id}')
+@app.put('/menu/{item_id}', dependencies=[Depends(JWTBearer())])
 async def update_menu(item_id: int, new_name: str):
     for menu_item in data['menu']:
         if menu_item['id'] == item_id:
@@ -40,7 +65,7 @@ async def update_menu(item_id: int, new_name: str):
         write_file.close()
     return {'message':'Data changed successfully'}
 
-@app.delete('/menu/{item_id}')
+@app.delete('/menu/{item_id}', dependencies=[Depends(JWTBearer())])
 async def delete_menu(item_id: int):
     for menu_item in data['menu']:
         if menu_item['id'] == item_id:
