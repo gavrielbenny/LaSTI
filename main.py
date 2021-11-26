212e2494b2387845d02738e0414e39cdfad816ea
gavrielsusanto <bennygavriel@gmail.com>
2021-11-26 18:05:47 +0700

    First Commit

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from fastapi import FastAPI, HTTPException, Depends
 from auth.auth_handler import signJWT, get_password_hash, verify_password
 from auth.auth_bearer import JWTBearer
 
-with open("menu.json", "r") as read_file:
+with open("presence.json", "r") as read_file:
     data = json.load(read_file)
 with open("user.json", "r") as read_file2:
     data2 = json.load(read_file2)
@@ -32,46 +32,24 @@ async def write_user(username : str, password : str):
 
 @app.get('/')
 def root():
-    return{'Menu':'Item'}
+    return{'Presence':'Item'}
 
-@app.get('/menu/{item_id}', dependencies=[Depends(JWTBearer())])
-async def read_menu(item_id: int):
-    for menu_item in data['menu']:
-        if menu_item['id'] == item_id:
-            return menu_item
+@app.get('/presence/{user_id}', dependencies=[Depends(JWTBearer())])
+async def read_presence(user_id: int):
+    for presence_user in data['presence']:
+        if presence_user['id'] == user_id:
+            return presence_user
     raise HTTPException(
         status_code=404, detail=f'Item not found'
         )
 
-@app.post('/menu/{item_id}/{item_name}', dependencies=[Depends(JWTBearer())])
-async def write_menu(name: str):
-    item_id = len(data['menu'])+1
-    newdata = {'id': item_id, 'name' : name}
-    if(item_id > 1):
-        data['menu'].append(newdata)
-        with open("menu.json", "w") as write_file:
+@app.post('/presence/{user_id}', dependencies=[Depends(JWTBearer())])
+async def write_presence(name: str, date: str, time: str):
+    user_id = len(data['presence'])+1
+    newdata = {'id': user_id, 'name' : name, 'date' : date, 'time' : time}
+    if(user_id > 1):
+        data['presence'].append(newdata)
+        with open("presence.json", "w") as write_file:
             json.dump(data, write_file)
         write_file.close()
         return data
-        
-@app.put('/menu/{item_id}', dependencies=[Depends(JWTBearer())])
-async def update_menu(item_id: int, new_name: str):
-    for menu_item in data['menu']:
-        if menu_item['id'] == item_id:
-            menu_item['name'] = new_name
-        read_file.close()    
-        with open("menu.json", "w") as write_file:
-            json.dump(data, write_file,indent=4)
-        write_file.close()
-    return {'message':'Data changed successfully'}
-
-@app.delete('/menu/{item_id}', dependencies=[Depends(JWTBearer())])
-async def delete_menu(item_id: int):
-    for menu_item in data['menu']:
-        if menu_item['id'] == item_id:
-            data['menu'].remove(menu_item)
-        read_file.close()    
-        with open("menu.json", "w") as write_file:
-            json.dump(data, write_file)
-        write_file.close()
-    return {'message':'Data deleted successfully'}
